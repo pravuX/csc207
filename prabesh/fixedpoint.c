aa6a9b3dea24b1cdc02d02fb5557afc26f40ca56
Prabesh Subedi <prabesh.sb@gmail.com>
2023-08-24 22:10:48 +0545

    Add fixedpoint

--- prabesh/fixedpoint.c
+++ prabesh/fixedpoint.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <math.h>
+#define g(x) ((a3*x*x*x+a2*x*x+a0) / (-a1))
+float a3, a2, a1, a0;
 
 int main(void) {
-  puts("Lmao!");
+  float x0, x1, E, Er;
+  puts("Enter the coefficients a3, a2, a1, a0");
+  scanf("%f%f%f%f", &a3, &a2, &a1, &a0);
+  puts("Enter the initial guess and error tolerance");
+  scanf("%f%f", &x0, &E);
+  while(1) {
+    x1 = g(x0);
+    Er = fabs((x1-x0) / x1);
+    if (Er < E) {
+      printf("The Root is %f", x1);
+      break;
+    }
+    x0 = x1;
+  }
   return 0;
 }
